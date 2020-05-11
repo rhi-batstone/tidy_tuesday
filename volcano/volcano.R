27aa9a8f383d4a648405cfdb12e2029b04aff2e8
Rhi <rbatstone94@gmail.com>
2020-05-11 22:05:47 +0100

    Tidied files

--- volcano/volcano.R
+++ volcano/volcano.R
@@ -1,44 +1,24 @@
----
-title: "Tidy Tuesday Volcanoes!"
-author: "Rhibarb"
-output: html_document
----
-
-##### Using the volcano data from #TidyTuesday to plot all volcanoes that have erupted since the year 1800
-* Hover over to see the volcano name and year of last eruption
-
-```{r include=FALSE}
 library(tidyverse)
 library(leaflet)
-```
 
-
-```{r include=FALSE}
 volcano <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/volcano.csv')
 eruptions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/eruptions.csv')
 events <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/events.csv')
 tree_rings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/tree_rings.csv')
 sulfur <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/sulfur.csv')
-```
 
-```{r include=FALSE}
 # Creating volcano icon for leaflet plot
 volcano_icon <- makeIcon(
   iconUrl = "nature (1).svg",
   iconWidth = 19, iconHeight = 48,
   iconAnchorX = 0, iconAnchorY = 0
 )
-```
 
-
-```{r include=FALSE}
 #making volcano data year if eruption numeric
 volcano <- volcano %>%  
   mutate(last_eruption_year = (as.numeric(as.character(last_eruption_year))))
-```
 
 
-```{r echo=FALSE}
 # Leaflet plot
 volcano %>%  
   #To reduce the number of icons on the map
@@ -56,6 +36,3 @@ volcano %>%
     # hover over labels as name and year of eruption
     label = ~paste0(volcano_name, ", ", last_eruption_year)
   )
-
-```
-
